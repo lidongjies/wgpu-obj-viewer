2e0567fa60668a022cd4aa97456a8c23aa15c713
lidongjies <lidongjies@gmail.com>
2022-05-07 13:23:53 +0800

    feat: Support depth buffer

--- src/lib.rs
+++ src/lib.rs
@@ -98,6 +98,7 @@ pub struct State {
     instance_buffer: wgpu::Buffer,
 
     diffuse_bind_group: wgpu::BindGroup,
+    depth_texture: texture::Texture,
 
     camera_controller: CameraController,
     camera_staging: CameraStaging,
@@ -298,7 +299,13 @@ impl State {
                 unclipped_depth: false,
                 conservative: false,
             },
-            depth_stencil: None,
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: texture::Texture::DEPTH_FORMAT,
+                depth_write_enabled: true,
+                depth_compare: wgpu::CompareFunction::Less,
+                stencil: wgpu::StencilState::default(),
+                bias: wgpu::DepthBiasState::default(),
+            }),
             multisample: wgpu::MultisampleState {
                 count: 1,
                 mask: !0,
@@ -321,6 +328,10 @@ impl State {
 
         let num_indices = INDICES.len() as u32;
 
+
+        let depth_texture = texture::Texture::create_depth_texture(&device, &config, "depth_texture");
+
+
         Self {
             surface,
             device,
@@ -337,6 +348,7 @@ impl State {
             instance_buffer,
 
             diffuse_bind_group,
+            depth_texture,
 
             camera_staging,
             camera_controller,
@@ -352,6 +364,7 @@ impl State {
             self.config.width = new_size.width;
             self.config.height = new_size.height;
             self.surface.configure(&self.device, &self.config);
+            self.depth_texture = texture::Texture::create_depth_texture(&self.device, &self.config, "depth_buffer");
         }
     }
 
@@ -404,7 +417,14 @@ impl State {
                         store: true,
                     },
                 }],
-                depth_stencil_attachment: None,
+                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                    view: &self.depth_texture.view,
+                    depth_ops: Some(wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(1.0),
+                        store: true,
+                    }),
+                    stencil_ops: None
+                }),
             });
             render_pass.set_pipeline(&self.render_pipeline);
             render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
